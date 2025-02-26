a151440cf9262d4fae2151066b98c19ea728b44c
mehmedsherif <mehmedali18@abv.bg>
2025-02-26 20:46:04 +0200

    Commit

---
+++ 03.EntityRelations/02.Football Betting/P02_FootballBetting.Common/ApplicationCommonConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P02_FootballBetting.Common
+{
+    public static class ApplicationCommonConfiguration
+    {
+        public const string ConnectionString = @"Server=LAPTOP-642N6SM1\MSSQLSERVER01;Database=FootballBetting2025;Trusted_Connection=True;";
+    }
+}
