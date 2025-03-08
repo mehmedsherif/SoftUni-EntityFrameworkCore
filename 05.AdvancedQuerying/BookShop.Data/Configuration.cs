6f6e3123c4ea9f33b7136cb91b42b6be4f9e8cb6
mehmedsherif <mehmedali18@abv.bg>
2025-03-08 23:15:55 +0200

    Upload

---
+++ 05.AdvancedQuerying/BookShop.Data/Configuration.cs
@@ -0,0 +1,8 @@
+﻿namespace BookShop.Data
+{
+    internal class Configuration
+    {
+        internal static string ConnectionString
+            => "Server=LAPTOP-642N6SM1\\MSSQLSERVER01;Database=BookShop;Integrated Security=True;";
+    }
+}
---
+++ 05.AdvancedQuerying/BookShop.Data/EntityConfiguration/AuthorConfiguration.cs
@@ -0,0 +1,24 @@
+﻿namespace BookShop.Data.EntityConfiguration
+{
+    using Models;
+
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+    using static Common.EntityValidationConstants.Author;
+
+    internal class AuthorConfiguration : IEntityTypeConfiguration<Author>
+    {
+        public void Configure(EntityTypeBuilder<Author> builder)
+        {
+            builder.HasKey(e => e.AuthorId);
+
+            builder.Property(e => e.FirstName)
+                .IsRequired(false)
+                .HasMaxLength(AuthorFirstNameMaxLength);
+
+            builder.Property(e => e.LastName)
+                .IsRequired()
+                .HasMaxLength(AuthorLastNameMaxLength);
+        }
+    }
+}
